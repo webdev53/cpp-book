adc349829c5fe83b168bd577c26c63828745dd75
Web Dev <web.2019.dev@gmail.com>
2022-06-07 07:26:56 +0545

    area of circle added

--- lesson1_basics/main.cpp
+++ lesson1_basics/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
@@ -18,11 +19,28 @@ int main(){
 
   //* sum of all natural numbers
   // sum = n(n+1)/2
-  int n, sum;
-  cout<<"Enter a number"<<endl;
-  cin>>n;
-  sum = n*(n+1)/2;
-  cout<<"Sum of n numbers is: "<< sum <<endl;;
+  // int n, sum;
+  // cout<<"Enter a number"<<endl;
+  // cin>>n;
+  // sum = n*(n+1)/2;
+  // cout<<"Sum of n numbers is: "<< sum <<endl;;
+
+  //* roots of a quadratic equation
+  // int a, b, c, r1, r2;
+  // cout<<"Enter the value of a, b, c"<<endl;
+  // cin>>a>>b>>c;
+  // r1 = (-b+sqrt(b*b - 4*a*c)/(2*a));
+  // r2 = (-b-sqrt(b*b - 4*a*c)/(2*a));
+  // cout<<"The value of r1 : " <<r1<<endl;
+  // cout<<"The value of r2 : " <<r2<<endl;
+
+  //* area of a circle
+  float r, a;
+  cout<<"Enter the value of radius of a circle"<<endl;
+  cin>>r;
+  // a = 22/7.0*r*r;
+  a = (float)22/7*r*r;
+  cout<<"Area of a circle : " << a<<endl;
 
 
  return 0;
