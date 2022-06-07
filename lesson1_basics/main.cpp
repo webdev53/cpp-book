7c20b8ae86ab231fdece20265e53964e33e44049
Web Dev <web.2019.dev@gmail.com>
2022-06-07 07:08:18 +0545

    more expressions operator performed

--- lesson1_basics/main.cpp
+++ lesson1_basics/main.cpp
@@ -3,9 +3,27 @@ using namespace std;
 
 int main(){
 
-  string name;
-  cout << "May i know your name?" << endl;
-  getline(cin, name);
-  cout << "Welcome Mr./Ms. " << name << endl;
+//* basic cin statement
+  // string name;
+  // cout << "May i know your name?" << endl;
+  // getline(cin, name);
+  // cout << "Welcome Mr./Ms. " << name << endl;
+
+//* expression
+  // float b, h, a;
+  // cout<<"Enter base & height"<<endl;
+  // cin>>b>>h;
+  // a = (b*h)/2;
+  // cout<<"Area of a triangle is: " << a;
+
+  //* sum of all natural numbers
+  // sum = n(n+1)/2
+  int n, sum;
+  cout<<"Enter a number"<<endl;
+  cin>>n;
+  sum = n*(n+1)/2;
+  cout<<"Sum of n numbers is: "<< sum <<endl;;
+
+
  return 0;
 }
