016591345aa8e8415849a7423874af8b0e96d82b
Web Dev <web.2019.dev@gmail.com>
2022-06-10 07:41:06 +0545

    more if else added

--- 02_conditional_statements/main.cpp
+++ 02_conditional_statements/main.cpp
@@ -4,15 +4,25 @@ using namespace std;
 
 int main(){
 
-  int x, y;
-  cout<<"Enter two numbers!"<<endl;
-  cin>>x>>y;
+  //* if else statement
+  // int x, y;
+  // cout<<"Enter two numbers!"<<endl;
+  // cin>>x>>y;
 
-  if(x>y){
-    cout<<"Max is " << x <<endl;
+  // if(x>y){
+  //   cout<<"Max is " << x <<endl;
 
+  // }else{
+  //   cout<<"Max is " << y <<endl;
+  // }
+
+  int roll;
+  cout<<"Enter your roll number"<<endl;
+  cin>>roll;
+  if(roll<1){
+    cout<<"invalid roll no" << endl;
   }else{
-    cout<<"Max is " << y <<endl;
+    cout<<"valid roll no" << endl;
   }
   
   return 0;
