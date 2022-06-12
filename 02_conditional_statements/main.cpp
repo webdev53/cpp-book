65301a7b3791224dd9358ff560bcf18045da7f3f
Web Dev <web.2019.dev@gmail.com>
2022-06-12 06:53:57 +0545

    else if ladder

--- 02_conditional_statements/main.cpp
+++ 02_conditional_statements/main.cpp
@@ -46,18 +46,33 @@ int main(){
   //  }
 
   //* else if
-   int a, b, c;
-   cout<<"Enter three numbers" << endl;
-   cin>>a>>b>>c;
+  //  int a, b, c;
+  //  cout<<"Enter three numbers" << endl;
+  //  cin>>a>>b>>c;
 
-   if(a>b && b>c){
-    cout<<"a is greater " <<endl;
+  //  if(a>b && b>c){
+  //   cout<<"a is greater " <<endl;
 
-   }else if(b>c){
-    cout<<"b is greater than c" <<endl;
-   }else{
-    cout<<"c is there"<<endl;
-   }
+  //  }else if(b>c){
+  //   cout<<"b is greater than c" <<endl;
+  //  }else{
+  //   cout<<"c is there"<<endl;
+  //  }
+
+  //* else if day ladder
+  int day;
+  cout<<"Enter day number"<<endl;
+  cin>>day;
+  if(day ==1){
+    cout<<"Sunday"<<endl;
+
+  }else if(day == 2){
+    cout<<"Monday"<<endl;
+  }else if(day == 3){
+    cout<<"Tuesday"<<endl;
+  }else{
+    cout<<"Invalid input"<<endl;
+  }
   
   return 0;
 }
